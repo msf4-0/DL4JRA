82e7a9d3d7481a990c7d12a6b17f72bd7464a873
nyeoWM <wmnicholasyeo@gmail.com>
2022-03-16 09:53:04 +0800

    Bug fixes 1. Changed to use the pretrained YOLO2 model from .deeplearning4j/models rather than saving and accessing a copy of the model on the server

--- server/src/main/java/com/dl4jra/server/odetection/PMRepository.java
+++ server/src/main/java/com/dl4jra/server/odetection/PMRepository.java
@@ -14,7 +14,8 @@ public class PMRepository {
 	 * 4. Pretrained model gridsize
 	 */
 	private static String rootpath = Paths.get("").toAbsolutePath().normalize().toString();
-	
+	private static String homepath = System.getProperty("user.home");
+
 	@SuppressWarnings("serial")
 	private static final HashMap<String, ArrayList<String>> classes = new HashMap<String, ArrayList<String>>() {{
 		put("tinyyolo", new ArrayList<String>(Arrays.asList("Aeroplane", "Bicycle", "Bird", "Boat", "Bottle", "Bus", "Car", "Cat", "Chair", "Cow", "DiningTable", "Dog", "Horse", "Motorbike", "Person", "Pottedplant", "Sheep", "Sofa", "Train", "Tvmonitor")));
@@ -23,6 +24,7 @@ public class PMRepository {
 	
 	public static ODModelConfigurationData GetPretrainedModelData(String modelname) throws Exception{
 		System.out.println(rootpath);
+		System.out.println(homepath);
 //		E:\SHRDC\DL4JRA2\DL4JRA\server/odmodels/tinyyolo.data
 		ODModelConfigurationData odModelConfigurationData;
 		if (modelname.equals("tinyyolo"))
@@ -30,7 +32,7 @@ public class PMRepository {
 					3, 13, 13, rootpath + "/odmodels/tinyyolo.data");
 		else if(modelname.equals("yolo2"))
 			odModelConfigurationData =  new ODModelConfigurationData(classes.get("yolo2"), 416, 416,
-					3, 13, 13, rootpath + "/odmodels/yolo2.data");
+					3, 13, 13, homepath + "/.deeplearning4j/models/yolo2/yolo2_dl4j_inference.v3.zip");
 		else
 			throw new Exception("Pretrained model not found");
 		return  odModelConfigurationData;
