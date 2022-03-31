e8899413d2c4a0c423adb81c43283adae97b307e
nyeoWM <wmnicholasyeo@gmail.com>
2022-03-31 14:14:28 +0800

    added debug messages for libraryloader

--- server/src/main/java/com/dl4jra/server/LibraryLoader.java
+++ server/src/main/java/com/dl4jra/server/LibraryLoader.java
@@ -3,9 +3,15 @@ package com.dl4jra.server;
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.opencv.opencv_java;
 
+import java.io.File;
+
 public class LibraryLoader {
 	// Load opencv library
 	public static void loadOpencvLibrary() {
+		final File f = new File(opencv_java.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+		System.out.println("\n\n\n Here lies \n\n\n some more \n\n\n");
+		System.out.println(f.getAbsolutePath());
+		System.out.println("\n\n\n Here lies \n\n\n some more \n\n\n");
 		Loader.load(opencv_java.class);
 	}
 }
